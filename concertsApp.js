23e72c1a299eaa6babe078aece656eb58b7f21a1
studenyp <studenyp@gmail.com>
2015-12-14 22:34:09 +0100

    Rewritten. Send mail button, add concert input

---
+++ concertsApp.js
@@ -0,0 +1,346 @@
+// 
+//  Object Concert keeps actual data in cookie, to allow easily control entrace state
+//  There is more helper functions, maybe TODO object
+//
+//  Author: Petr Studeny <dosmanak@centrum.cz>
+//
+function setCookie(name, value)
+{
+	/* dont ask for expiration in argument, use fix value */
+	days = 2
+	var date = new Date();
+	date.setTime(date.getTime()+days*24*60*60*1000); 
+	var expires = "; expires=" + date.toGMTString();
+	document.cookie = name+"=" + value+expires + ";path=/"; 
+}
+function getCookie(cname) {
+				var name = cname + "=";
+				var ca = document.cookie.split(';');
+				for(var i=0; i<ca.length; i++) {
+								var c = ca[i];
+								while (c.charAt(0)==' ') c = c.substring(1);
+								if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+				}
+				return 0;
+}
+function getCookiesContaining(cname) {
+				var values = new Array();
+				var name = cname + "=";
+				var ca = document.cookie.split(';');
+				for(var i=0; i<ca.length; i++) {
+								var c = ca[i];
+								while (c.charAt(0)==' ') c = c.substring(1);
+								if (c.indexOf(name) > 0) values.push(c.substring(c.indexOf('=')+1, c.length));
+				}
+				return values;
+}
+function deleteAllCookies() 
+{
+	var cookies = document.cookie.split(";");
+
+	for (var i = 0; i < cookies.length; i++) {
+		var cookie = cookies[i];
+		var eqPos = cookie.indexOf("=");
+		var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+		document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+	}
+	window.location.reload(false);
+}
+function slugify(text)
+{
+	return text.toString().toLowerCase()
+	.replace(/\s+/g, '_')           // Replace spaces with _
+	.replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+	.replace(/\-\-+/g, '_')         // Replace multiple - with single _
+	.replace(/-/g,'_')							// Replace - with _
+	.replace(/^-+/, '')             // Trim - from start of text
+	.replace(/-+$/, '');            // Trim - from end of text
+}
+
+//
+// Object Evening
+//
+function Evening(id)
+{
+	this.htmlObject = document.getElementById(id);
+	if (! this.htmlObject ) 
+	{
+		console.log('wrong html id used creating Evening');
+		return null;
+	}
+	this.defaultTitle = function()
+	{
+		d = new Date();
+		return "Koncert "+d.getDate()+". "+d.getMonth()+"., "+d.getHours()+"h";
+	}
+	this.formTitle = document.createElement("input");
+	this.formTitle.type = 'text';
+	this.formTitle.id = 'formTitle';
+	this.formTitle.value = this.defaultTitle();
+
+	this.formRows = document.createElement("input");
+	this.formRows.type = 'number';
+	this.formRows.id = 'formRows';
+	this.formRows.value = 4;
+
+	this.concerts = new Array();
+
+	this.getConcertValues = function()
+	{
+		var tuple = new Array();
+		var elem = document.getElementById('formTitle');
+		if ( ! elem ) 
+		{
+			console.log('formTitle undefined');
+			elem = defaultTitle(); 
+		}
+		tuple['title'] = elem.value;
+		elem = document.getElementById('formRows');
+		if ( ! elem ) 
+		{
+			console.log('formRow undefined');
+			elem = 4; 
+		}
+		tuple['rows'] = elem.value;
+		return tuple;
+	}
+	this.formButton = document.createElement("input");
+	this.formButton.type = 'button';
+	this.formButton.id = 'formButton';
+	this.formButton.value = 'Přidej';
+	var that = this;
+	this.formButton.onclick = function()
+	{
+		var formValues = that.getConcertValues();
+		var co = new Concert ( formValues['title'],formValues['rows'] );
+		that.concerts.unshift( co );
+		that.htmlObject.appendChild(co.drawTable()); 
+	}
+	this.htmlObject.appendChild(this.formTitle);
+	this.htmlObject.appendChild(this.formRows);
+	this.htmlObject.appendChild(this.formButton);
+	// That was just the beggingin, now try to search cookies for existing Concerts
+	var titlesCookie = getCookiesContaining("title");
+	var rowsCookie = getCookiesContaining("rows");
+	while (c = titlesCookie.shift())
+	{
+		this.concerts.push(new Concert(c, rowsCookie.shift()));
+		this.htmlObject.appendChild( concerts[concerts.length - 1].drawTable());
+		this.concerts[concerts.length - 1].loadCookies();
+	}
+
+	// Go through concerts go generate json to send to server
+	this.data = 'data={';
+	for (var i=0; i < this.concerts.length; i++)
+	{
+		if ( i > 0 ) this.data+= ',';
+		this.data += this.concerts[i].jsonify();
+	}
+	this.data += '}';
+	var that = this;
+
+	var sendto = document.createElement("input");
+	sendto.type = "text";
+	sendto.id = "SendMailTo";
+	//sendto.value = "info@jazzdock.cz";
+	sendto.value = "dosmanak@centrum.cz";
+	document.getElementById('resetCookies').appendChild(sendto);
+	var send = document.createElement("input");
+	send.type = "button";
+	send.class = "SendMail";
+	send.onclick = function () 
+	{ 
+		var to = document.getElementById("SendMailTo").value;
+		var data = 'mailto='+to+'\&'+that.data;
+		sendConcertsData(data); 
+	};
+	send.value = "Odeslat e-mail";
+	document.getElementById('resetCookies').appendChild(send);
+
+	console.log(getCookiesContaining("title"));
+	var resetButton = document.createElement("input");
+	resetButton.type = "button";
+	resetButton.value = "Resetovat stránku";
+	resetButton.onclick = function() { deleteAllCookies(); }
+	document.getElementById('resetCookies').appendChild(resetButton);
+}
+
+function sendConcertsData(data)
+{
+	var xhttp = new XMLHttpRequest();
+	xhttp.open("POST", "vstupar.php", true);
+	xhttp.onreadystatechange = function() {
+		if (xhttp.readyState == 4 && xhttp.status == 200) {
+			document.write(xhttp.responseText);
+		}
+	}
+	xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	xhttp.send(data);
+}
+
+/***
+** Object concert:
+** one concert must have variables:
+**   name string
+**   rows integer
+**   price[rows] array of integers
+**   count[rows] array of integers
+** one concert must have methods:
+**   sumPrice(row)
+**   changeValBut(row, val)
+** one concert mauste have constructor
+**   new Concert(name,rows) - draw table with buttons and correct variables in it.
+**     create one concert with given name and rows.
+***/
+function Concert(_name,_rows)
+{
+	this.title = _name;
+	this.name = slugify(_name);
+	this.rows = _rows;
+	setCookie(this.name+"-title", this.title); // to be able to recreate from cookies
+	setCookie(this.name+"-rows", this.rows);
+	this.price = new Array(this.rows);
+	this.count = new Array(this.rows);
+	for (var i=0; i < this.rows; i++) {
+		this.price[i]=0;
+		this.count[i]=0;
+	}
+	this.setPrice = function(row, price)
+	{
+		if (isNaN(price)) { alert ("wrong call to setPrice"); return;};
+		this.price[row] = price;
+		//console.log(this.name+"-price"+row);
+		//console.log(document.getElementById(this.name+"-price"+row));
+		document.getElementById(this.name+"-price"+row).value = price;
+		document.getElementById(this.name+"-sumPrice"+row).innerHTML = this.sumPrice(row);
+		document.getElementById(this.name+"-totalPrice").innerHTML = this.getTotalPrice();
+		setCookie(this.name+"-price"+row, price);
+	}
+	this.setCount = function(row, count)
+	{
+		if (isNaN(count)) { alert ("wrong call to setCount"); return;};
+		this.count[row] = count;
+		document.getElementById(this.name+"-count"+row).innerHTML = this.count[row];
+		document.getElementById(this.name+"-sumPrice"+row).innerHTML = this.sumPrice(row);
+		document.getElementById(this.name+"-totalPrice").innerHTML = this.getTotalPrice();
+		document.getElementById(this.name+"-totalCount").innerHTML = this.getTotalCount();
+		setCookie(this.name+"-count"+row, count);
+	}
+	this.sumPrice = function(row)
+	{
+		return this.price[row]*this.count[row];
+	}
+	this.getTotalPrice = function()
+	{
+		var totalPrice = 0;
+		for (var i=0;i<this.rows;i++)
+		{
+			totalPrice += this.sumPrice(i);
+		}
+		return totalPrice;	
+	}
+	
+	this.getTotalCount = function()
+	{
+		var totalCount = 0;
+		for (var i=0;i<this.rows;i++)
+		{
+			totalCount += this.count[i];
+		}
+		return totalCount;
+	}
+	
+	this.changeValBut = function(row,val){
+		this.setCount(row,this.count[row] + val);
+	}
+	this.drawButton = function(row,val)
+	{
+		var input = document.createElement("input");
+		input.type = "button";
+		input.id = row+this.name+val;
+		var label = val.toString();
+		if (label.charAt(0) != "-") { label = "+"+label; };
+		input.className = "modifier modifier"+val;
+		input.value = label;
+		//console.log("this, pri incializaci buttonu: ",this.name);
+		var that = this;
+		input.onclick = function() { that.changeValBut(row,val); }
+		return input;
+	}
+	this.drawTable = function()
+	{
+		var envelope = document.createElement("div");
+		envelope.id = this.name;
+		var title = document.createElement("h2");
+		title.name = this.title;
+		title.innerHTML = this.title;
+		var feeTable = document.createElement("table");
+		var th = feeTable.insertRow(-1);
+		th.className = "tableHeader";
+		var hcount = th.insertCell(-1);
+		hcount.innerHTML = "<b>Počet</b>";
+		var hsumPrice = th.insertCell(-1);
+		hsumPrice.innerHTML = "<b>Součet</b>";
+		var hprice = th.insertCell(-1);
+		hprice.innerHTML = "<b>Cena</b>";
+		for (c=0; c < 4; c++){th.insertCell(-1);};
+		for (r=0; r < this.rows; r++){
+			var tr = feeTable.insertRow(-1);
+			var countCell = tr.insertCell(-1);
+			countCell.id = this.name+"-count"+r;
+			countCell.name = this.name+"-count"+r;
+			countCell.innerHTML = this.count[r];
+			var sumPriceCell = tr.insertCell(-1);
+			sumPriceCell.id = this.name+"-sumPrice"+r;
+			sumPriceCell.innerHTML = this.sumPrice(r);
+			var priceCell = tr.insertCell(-1);
+			var priceCellInput = document.createElement("input");
+			priceCellInput.type = "number";
+			priceCellInput.id = this.name+"-price"+r;
+			priceCellInput.name = this.name+"-price"+r;
+			priceCellInput.className = "priceCol";
+			priceCellInput.value = this.price[r];
+			priceCellInput.size = 4;
+			console.log("init: ",r);
+			//this.r = r;				
+			var that = this;
+			priceCellInput.onblur = function(){
+				var roww = this.id.replace(/.*-price/, "");
+				that.setPrice(roww, this.value);
+			}
+			//priceCellInput.onblur = () => this.setPrice(r, priceCellInput.value);
+			priceCell.appendChild(priceCellInput);
+			//tr.insertCell(-1).appendChild(this.drawButton(r,-2));
+			tr.insertCell(-1).appendChild(this.drawButton(r,-1));
+			tr.insertCell(-1).appendChild(this.drawButton(r,+1));
+			tr.insertCell(-1).appendChild(this.drawButton(r,+2));
+			tr.insertCell(-1).appendChild(this.drawButton(r,+4));
+			console.log("Processing row "+r);
+		}
+		var sumTotaltr = feeTable.insertRow(-1);
+		sumTotaltr.innerHTML='<td id="'+this.name+'-totalCount">0</td>\
+		<td id="'+this.name+'-totalPrice">0</td><td colspan="5">⇐Celkem</td>';
+		
+		envelope.appendChild(title);
+		envelope.appendChild(feeTable);
+		
+		return envelope;
+	}
+	this.loadCookies = function()
+	{
+		/* fill from cookie */
+		for (r=0; r < this.rows; r++){
+			var priceRow = parseInt(getCookie(this.name+"-price"+r));
+			if (priceRow == 0) { priceRow = 50*r; }
+			this.setPrice(r,priceRow);
+			this.setCount(r,parseInt(getCookie(this.name+"-count"+r)));
+		}
+	}
+	this.jsonify = function()
+	{
+		//var jsonstring = '{"'+this.name+'":{ "title": "'+this.title+'", "prices": ['+this.price+'], "counts": ['+this.count+']}}';
+		var jsonstring = '"'+this.name+'":{ "title": "'+encodeURIComponent(this.title)+'", "prices": ['+this.price+'], "counts": ['+this.count+']}';
+		return jsonstring;
+	}
+}
+
--- vstupar.php
+++ vstupar.php
@@ -1,7 +1,7 @@
 <!--
    vstupar.php
    
-   Copyright 2015 Petr Studený <dosmanak@dosbook>
+   Copyright 2015 Petr Studený <dosmanak@centrum.cz>
    
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -29,7 +29,7 @@
 	<title>Vstupařův průvodce večerem</title>
 	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
 	<meta content='width=device-width, initial-scale=0.7, maximum-scale=0.9, user-scalable=0' name='viewport' />
-	<style>
+	<style type="text/css">
 		body { font-family: Helvetica }
 		#concerts { padding: 2%; }
 		#concerts table { width: 100%; font-size: 230%; table-layout: fixed}
@@ -44,293 +44,26 @@
 		*/
 		#eraser input{ font-size: 150%; line-height: 150% }
 	</style>
-	<script type="text/javascript">
-	function setCookie(name, value)
-	{
-		/* dont ask for expiration in argument, use fix value */
-		days = 2
-		var date = new Date();
-		date.setTime(date.getTime()+days*24*60*60*1000); 
-		var expires = "; expires=" + date.toGMTString();
-		document.cookie = name+"=" + value+expires + ";path=/"; 
-	}
-	function getCookie(cname) {
-					var name = cname + "=";
-					var ca = document.cookie.split(';');
-					for(var i=0; i<ca.length; i++) {
-									var c = ca[i];
-									while (c.charAt(0)==' ') c = c.substring(1);
-									if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
-					}
-					return 0;
-	}
-	function deleteAllCookies() 
-	{
-		var cookies = document.cookie.split(";");
-
-		for (var i = 0; i < cookies.length; i++) {
-			var cookie = cookies[i];
-			var eqPos = cookie.indexOf("=");
-			var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-			document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
-		}
-		window.location.reload(false);
-	}
-	function slugify(text)
-	{
-	  return text.toString().toLowerCase()
-		.replace(/\s+/g, '-')           // Replace spaces with -
-		.replace(/[^\w\-]+/g, '')       // Remove all non-word chars
-		.replace(/\-\-+/g, '-')         // Replace multiple - with single -
-		.replace(/^-+/, '')             // Trim - from start of text
-		.replace(/-+$/, '');            // Trim - from end of text
-	}
-	function defaultName()
-	{
-		d = newDate();
-		return "Koncert "+d.getDay()+". "+d.getMonth()+"., "+d.getHours()+"h";
-	}
-
-	function addConcertForm() //TBD
-	{
-		newConcert = document.createElement("input");
-		newConcert.type = "text";
-		newConcert.id = "title";
-		newConcert.value = defaultName();
-		newVariants = document.createElement("input");
-		newConcert.type = "text";
-		newVariants.id = "rows";
-		newVariants.value = 5;
-		newButton = document.createElement("input");
-		newButton.type = "button";
-		newButton.value = "Přidej";
-    newButton.onclick = function() { new Concert() };
-	}
-
-	function createConcert()
-	{
-		var data = 'data={';
-		concerts = document.getElementById("concerts");
-		c1 = new Concert("První",5);
-		concerts.appendChild(c1.drawTable());
-		c1.loadCookies();	
-		data += c1.jsonify();
-		c2 = new Concert("Druhý",5);
-		concerts.appendChild(c2.drawTable());
-		c2.loadCookies();
-		data += ','+c2.jsonify();
-		c3 = new Concert("Special",3);
-		concerts.appendChild(c3.drawTable());		
-		c3.loadCookies();
-		data += ','+c3.jsonify();
-		data += '}';
-
-		send = document.createElement("input");
-		send.type = "button";
-		send.class = "SendMail";
-		send.onclick = function () { sendConcertsData(data); };
-		send.value = "Odeslat e-mail";
-		concerts.appendChild(send);
-
-		
-	}
-
-	function sendConcertsData(data)
-	{
-		var xhttp = new XMLHttpRequest();
-		xhttp.open("POST", "vstupar.php", true);
-		xhttp.onreadystatechange = function() {
-			if (xhttp.readyState == 4 && xhttp.status == 200) {
-				document.write(xhttp.responseText);
-			}
-		}
-		xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-		xhttp.send(data);
-	}
-
-	/***
-	** Object concert:
-	** one concert must have variables:
-	**   name string
-	**   rows integer
-	**   price[rows] array of integers
-	**   count[rows] array of integers
-	** one concert must have methods:
-	**   sumPrice(row)
-	**   changeValBut(row, val)
-	** one concert mauste have constructor
-	**   new Concert(name,rows) - draw table with buttons and correct variables in it.
-	**     create one concert with given name and rows.
-	***/
-	function Concert(_name,_rows)
-	{
-		this.title = _name;
-		this.name = slugify(_name);
-		this.rows = _rows;
-		this.price = new Array(this.rows);
-		this.count = new Array(this.rows);
-    for (var i=0; i < this.rows; i++) {
-      this.price[i]=0;
-      this.count[i]=0;
-    }
-		this.setPrice = function(row, price)
-		{
-			if (isNaN(price)) { alert ("wrong call to setPrice"); return;};
-			this.price[row] = price;
-			//console.log(this.name+"-price"+row);
-			//console.log(document.getElementById(this.name+"-price"+row));
-			document.getElementById(this.name+"-price"+row).value = price;
-			document.getElementById(this.name+"-sumPrice"+row).innerHTML = this.sumPrice(row);
-			document.getElementById(this.name+"-totalPrice").innerHTML = this.getTotalPrice();
-			setCookie(this.name+"-price"+row, price);
-		}
-		this.setCount = function(row, count)
-		{
-			if (isNaN(count)) { alert ("wrong call to setCount"); return;};
-			this.count[row] = count;
-			document.getElementById(this.name+"-count"+row).innerHTML = this.count[row];
-			document.getElementById(this.name+"-sumPrice"+row).innerHTML = this.sumPrice(row);
-			document.getElementById(this.name+"-totalPrice").innerHTML = this.getTotalPrice();
-			document.getElementById(this.name+"-totalCount").innerHTML = this.getTotalCount();
-			setCookie(this.name+"-count"+row, count);
-		}
-		this.sumPrice = function(row)
-		{
-			return this.price[row]*this.count[row];
-		}
-		this.getTotalPrice = function()
-		{
-			var totalPrice = 0;
-			for (var i=0;i<this.rows;i++)
-			{
-				totalPrice += this.sumPrice(i);
-			}
-			return totalPrice;	
-		}
-		
-		this.getTotalCount = function()
-		{
-			var totalCount = 0;
-			for (var i=0;i<this.rows;i++)
-			{
-				totalCount += this.count[i];
-			}
-			return totalCount;
-		}
-		
-		this.changeValBut = function(row,val){
-			this.setCount(row,this.count[row] + val);
-		}
-		this.drawButton = function(row,val)
-		{
-			var input = document.createElement("input");
-			input.type = "button";
-			input.id = row+this.name+val;
-			var label = val.toString();
-			if (label.charAt(0) != "-") { label = "+"+label; };
-			input.className = "modifier modifier"+val;
-			input.value = label;
-			//console.log("this, pri incializaci buttonu: ",this.name);
-			var that = this;
-			input.onclick = function() { that.changeValBut(row,val); }
-			return input;
-		}
-		this.drawTable = function()
-		{
-			var envelope = document.createElement("div");
-			envelope.id = this.name;
-			var title = document.createElement("h2");
-			title.name = this.title;
-			title.innerHTML = this.title;
-			var feeTable = document.createElement("table");
-			var th = feeTable.insertRow(-1);
-			th.className = "tableHeader";
-			var hcount = th.insertCell(-1);
-			hcount.innerHTML = "<b>Počet</b>";
-			var hsumPrice = th.insertCell(-1);
-			hsumPrice.innerHTML = "<b>Součet</b>";
-			var hprice = th.insertCell(-1);
-			hprice.innerHTML = "<b>Cena</b>";
-			for (c=0; c < 4; c++){th.insertCell(-1);};
-			for (r=0; r < this.rows; r++){
-				var tr = feeTable.insertRow(-1);
-				var countCell = tr.insertCell(-1);
-				countCell.id = this.name+"-count"+r;
-				countCell.name = this.name+"-count"+r;
-				countCell.innertHTML = this.count[r];
-				var sumPriceCell = tr.insertCell(-1);
-				sumPriceCell.id = this.name+"-sumPrice"+r;
-				sumPriceCell.innerHTML = this.sumPrice(r);
-				var priceCell = tr.insertCell(-1);
-				var priceCellInput = document.createElement("input");
-				priceCellInput.type = "number";
-				priceCellInput.id = this.name+"-price"+r;
-				priceCellInput.name = this.name+"-price"+r;
-				priceCellInput.className = "priceCol";
-				priceCellInput.value = this.price[r];
-				priceCellInput.size = 4;
-				console.log("init: ",r);
-				//this.r = r;				
-				var that = this;
-				priceCellInput.onblur = function(){
-					roww = this.id.replace(/\D*/, "");
-					that.setPrice(roww, this.value);
-				}
-				//priceCellInput.onblur = () => this.setPrice(r, priceCellInput.value);
-				priceCell.appendChild(priceCellInput);
-				//tr.insertCell(-1).appendChild(this.drawButton(r,-2));
-				tr.insertCell(-1).appendChild(this.drawButton(r,-1));
-				tr.insertCell(-1).appendChild(this.drawButton(r,+1));
-				tr.insertCell(-1).appendChild(this.drawButton(r,+2));
-				tr.insertCell(-1).appendChild(this.drawButton(r,+4));
-				console.log("Processing row "+r);
-			}
-			var sumTotaltr = feeTable.insertRow(-1);
-			sumTotaltr.innerHTML='<td id="'+this.name+'-totalCount">0</td>\
-			<td id="'+this.name+'-totalPrice">0</td><td colspan="5">⇐Celkem</td>';
-			
-			envelope.appendChild(title);
-			envelope.appendChild(feeTable);
-			
-			return envelope;
-		}
-		this.loadCookies = function()
-		{
-			/* fill from cookie */
-			for (r=0; r < this.rows; r++){
-				var priceRow = parseInt(getCookie(this.name+"-price"+r));
-				if (priceRow == 0) { priceRow = 50*r; }
-				this.setPrice(r,priceRow);
-				this.setCount(r,parseInt(getCookie(this.name+"-count"+r)));
-			}
-		}
-		this.jsonify = function()
-		{
-			//var jsonstring = '{"'+this.name+'":{ "title": "'+this.title+'", "prices": ['+this.price+'], "counts": ['+this.count+']}}';
-			var jsonstring = '"'+this.name+'":{ "title": "'+this.title+'", "prices": ['+this.price+'], "counts": ['+this.count+']}';
-			return jsonstring;
-		}
-	}
-
-	</script>
+	<script src="concertsApp.js" type="text/javascript"></script>
 </head>
 
-<body id="main" onLoad="createConcert()">
-	<div id="concertForm"></div>
+<body onload="Evening('concerts')">
+<div id="concerts">
 <?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST')
 {
+//dos	require 'class.phpmailer.php';
 	$datajson = ($_POST["data"]);
 	$data = json_decode($_POST["data"],true);
 	//var_dump($data);
-	echo "<pre>\n";
-	$body = '';
-	setlocale(LC_TIME, "cs_CZ");
-	$body .= strftime("Vstupne do JazzDocku za %A %d. %m. %G %R\n");
+	$body = '<pre>';
+	setlocale(LC_TIME, "cs_CZ.UTF-8");
+	$body .= "Vstupné do JazzDocku za ".strftime("%A %d. %m. %G %R\n");
+	$total = 0;
 	foreach ($data as $key => $value) {
-		$body .= $value['title'];
+		$body .= urldecode($value['title']);
 		$body .= ":\n";
-		$body .= "\tCena:\tPocet:\tCelkem\n";
+		$body .= "\tCena:\tPočet:\tCelkem\n";
 		$sum = 0;
 		for ($i = 0; ($i < count($value['prices'])); $i++)
 		{
@@ -340,24 +73,42 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 			$sum += $value['counts'][$i]*$value['prices'][$i];
 			$body .= "\n";
 		}
-		$body .= "\t\tSoucet:\t$sum\n";
+		$body .= "\t\tSoučet:\t$sum\n";
+		$total += $sum;
 	}
+	$body .= "Součet za celý večer:\t$total";
+	$body .= '</pre>';
 	echo $body;
-	echo "</pre>";
-	$sent = mail("Petr Studeny <dosmanak@centrum.cz>",strftime("JZD Vstupne %d.%m.%G"),$body);
-	echo "Mail odeslan je $sent";
+	$to = ($_POST["mailto"]);
+	$headers  = 'MIME-Version: 1.0' . "\r\n";
+	$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+	$headers .= 'From: =?utf-8?Q?Vstupa=C5=99=20JZD?= <vstupar@jazztecetera.cz>'. "\r\n";
+	if( mail($to, 'JZD Vstupné '.strftime('%d.%m.%G'), $body, $headers) ) 
+	{
+      echo 'E-mail byl odeslán.';
+  }else{
+  	  echo 'E-mail nebyl odeslán';
+  }
+	//$to = "Petr Studeny <dosmanak@centrum.cz>";
+//dos 	$mail = new PHPMailer;
+//dos 	$mail->isMail();
+//dos 	$mail->setFrom('vstupar@jazztecetera.cz', 'Vstupar JZD');
+//dos 	$mail->addAddress($to);
+//dos 	$mail->Subject = 'JZD Vstupné '.strftime('%d.%m.%G');
+//dos 	$mail->msgHTML($body);
+//dos 	if (!$mail->send()) {
+//dos 		    echo "Chyba při odesílání: " . $mail->ErrorInfo;
+//dos 	} else {
+//dos 		    echo "E-mail odeslán na ".$to."!\n".$mail->ErrorInfo;
+//dos 	}
 }
 else
 {
-?>
-	  <div id="concerts"></div>
-<?php
 }
 ?>
-	<div id="eraser">
-		<input type="button" value="Resetovat stránku" onclick="deleteAllCookies()"/>
-	</div>
-	
+</div>
+<div id="sendMail"></div>
+<div id="resetCookies"></div>
 </body>
 
 </html>
